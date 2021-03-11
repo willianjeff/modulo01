65189eedea85f155212f6848da798a5fc6afbfa9
willianjeff <willianib200@gmail.com>
2021-03-11 19:57:36 -0300

    arquivo da media criado

---
+++ exercicio4.py
@@ -0,0 +1,14 @@
+numero1 = 0
+numero2 = 0
+numero3 = 0
+numero4 = 0
+media = 0
+
+numero1 = int(input("Digite a primeira nota: "))
+numero2 = int(input("Digite a segunda nota: "))
+numero3 = int(input("Digite a terceira nota: "))
+numero4 = int(input("Digite a quarta nota: "))
+
+media = (numero1+numero2+numero3+numero4)/4
+
+print("A media é igual a : ", media)
